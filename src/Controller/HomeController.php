14a558b335e9494c705be5f56bd94a3aa2cb86b7
Tomasz Wnuk <tomasz.wnuk1@gmail.com>
2023-12-04 21:37:18 +0100

    Home Controller

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route(path: '/', name: 'home', methods: ['GET'])]
+    public function list(): Response
+    {
+        return new Response('Witaj świecie ');
+    }
+}
