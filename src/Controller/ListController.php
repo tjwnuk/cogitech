78652fd1eb3e655e1f47ded935015f6c2057b97f
Tomasz Wnuk <tomasz.wnuk1@gmail.com>
2023-12-05 17:05:20 +0100

    fetch authors and mix with articles

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -11,7 +11,7 @@ use Symfony\Component\VarDumper\VarDumper;
 
 class ListController extends AbstractController
 {
-    public function fetchData(string $url)
+    public function fetchArticles(string $url) : array
     {
         $client = HttpClient::create();
         $response = $client->request('GET', $url);
@@ -25,11 +25,58 @@ class ListController extends AbstractController
         return [];
     }
 
+    public function fetchAuthors(string $url) : array
+    {
+        $client = HttpClient::create();
+        $response = $client->request('GET', $url);
+
+        if ($response->getStatusCode() == 200)
+        {
+            $authors = $response->toArray();
+            return $authors;
+        }
+
+        return [];
+    }
+
+    public function mixArticlesWithAuthors(array $articles, array $authors) : array
+    {
+        $result = [];
+
+        foreach ($articles as $key => $article) 
+        {
+            $userId = $article['userId'];
+            $name = "";
+
+            foreach($authors as $author)
+            {
+                if($userId == $author['id'])
+                {
+                    $name = $author['name'];
+                }
+            }
+
+            $newElement = [
+                'id' => $article['id'],
+                'name' => $name,
+                'title' => $article['title'],
+                'body' => $article['body']
+            ];
+
+            array_push($result, $newElement);
+        }
+
+        return $result;
+    }
+
     #[Route(path: '/list', name: 'list', methods: ['GET'])]
     public function list(): Response
     {
-        $data = $this->fetchData('https://jsonplaceholder.typicode.com/posts');
+        $articles = $this->fetchArticles('https://jsonplaceholder.typicode.com/posts');
+        $authors = $this->fetchAuthors('https://jsonplaceholder.typicode.com/users');
         // $data = VarDumper::dump($data);
+
+        $data = $this->mixArticlesWithAuthors($articles, $authors);
         
         return $this->render('list.html.twig', [
             'data' => $data,
