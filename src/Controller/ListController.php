d2c8cbc1b8b32d9c6c3025d64aac20d71e6a6d51
Tomasz Wnuk <tomasz.wnuk1@gmail.com>
2023-12-10 00:18:03 +0100

    deleting items

--- src/Controller/ListController.php
+++ src/Controller/ListController.php
@@ -3,11 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 use Doctrine\Persistence\ManagerRegistry as PersistenceManagerRegistry;
-
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Security\Core\Security;
 
 use App\Entity\Article;
@@ -20,7 +21,7 @@ class ListController extends AbstractController
     }
     
     #[Route(path: '/lista', name: 'list', methods: ['GET'])]
-    public function list(PersistenceManagerRegistry $doctrine): Response
+    public function list(PersistenceManagerRegistry $doctrine, Request $request): Response
     {
         $data = [];
         $entityManager = $doctrine->getManager();
@@ -39,12 +40,39 @@ class ListController extends AbstractController
         else {
             $loggedIn = false;
         }
+
+        $message = null;
+        $session = $request->getSession();
+        $message = $session->getFlashBag()->get('info');
         
 
         return $this->render('list.html.twig', [
             'data' => $data,
             'username' => $username,
             'loggedIn' => $loggedIn,
+            'message' => $message,
         ]);
+    }
+
+    #[Route(path: '/lista/delete/{id}', name: 'delete', methods: ['GET'])]
+    public function delete($id, PersistenceManagerRegistry $doctrine, Request $request): Response
+    {
+
+        $entityManager = $doctrine->getManager();
+        $repo = $entityManager->getRepository(Article::class);
+
+        $entity = $repo->find($id);
+
+        if(!$entity)
+        {
+            throw $this->createNotFoundException("Not found record of id $id");
+        }
+
+        $entityManager->remove($entity);
+        $entityManager->flush();
+
+        $this->addFlash('info', "Item of id $id removed successfully.");
+
+        return $this->redirectToRoute('list');
     }
 }
